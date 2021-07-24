573573045842fa8709dbadeb364935dcc07a3b77
Tasawar Hussain <31658686+tasawar-hussain@users.noreply.github.com>
2021-07-25 00:27:18 +0500

    refactor config

--- config.py
+++ config.py
@@ -12,9 +12,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # Enable debug mode.
 DEBUG = True
 
-# Connect to the database
 SECRET_KEY = os.environ.get("SECRET_KEY")
 FLASK_ENV = os.environ.get("FLASK_ENV")
 FLASK_APP = os.environ.get("FLASK_APP")
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# Connect to the database
 SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+SQLALCHEMY_TRACK_MODIFICATIONS = False
